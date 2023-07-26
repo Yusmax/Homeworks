b999efbf8e51e88176c836c5ca4491b496477210
Yusmax <94967254+Yusmax@users.noreply.github.com>
2023-07-26 10:38:53 +0400

    HW2

---
+++ HW#2 UdpGroupMessenger/UDPmessenger/Client2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class UdpClientExample
+{
+    private const int ServerPort = 5000;
+    private static readonly IPAddress ServerIp = IPAddress.Parse("127.0.0.1");
+
+    static void Main()
+    {
+        using (var client = new UdpClient())
+        {
+            IPEndPoint serverEndPoint = new IPEndPoint(ServerIp, ServerPort);
+
+            Console.Write("Enter a nickname: ");
+            string nickname = Console.ReadLine();
+            byte[] nicknameData = Encoding.UTF8.GetBytes(nickname);
+            client.Send(nicknameData, nicknameData.Length, serverEndPoint);
+
+            Console.WriteLine("Enter 'stop' to stop the client.");
+
+            while (true)
+            {
+                Console.Write("Enter a message: ");
+                string message = Console.ReadLine();
+
+                if (message == "stop")
+                    break;
+
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                client.Send(data, data.Length, serverEndPoint);
+            }
+        }
+    }
+}
---
+++ HW#2 UdpGroupMessenger/UDPmessenger/Servet/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class UdpServerExample
+{
+    private const int ServerPort = 5000;
+
+    static void Main()
+    {
+        UdpClient server = new UdpClient(ServerPort);
+        Dictionary<IPEndPoint, string> clientNicknames = new Dictionary<IPEndPoint, string>();
+
+        Console.WriteLine("UDP Server is listening...");
+
+        try
+        {
+            IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, ServerPort);
+            while (true)
+            {
+                byte[] data = server.Receive(ref clientEndPoint);
+                string message = Encoding.UTF8.GetString(data);
+
+                if (!clientNicknames.ContainsKey(clientEndPoint))
+                {
+                    // If the client is new and doesn't have a nickname, set it.
+                    string nickname = message;
+                    clientNicknames.Add(clientEndPoint, nickname);
+                    Console.WriteLine($"Nickname set for {clientEndPoint}: {nickname}");
+                }
+                else
+                {
+                    // If the client already has a nickname, display the message with the nickname.
+                    string nickname = clientNicknames[clientEndPoint];
+                    Console.WriteLine($"{nickname}: {message}");
+                }
+            }
+        }
+        catch (SocketException ex)
+        {
+            Console.WriteLine($"Error receiving message: {ex.Message}");
+        }
+        finally
+        {
+            server.Close();
+        }
+    }
+}
